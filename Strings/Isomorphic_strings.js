4036719d95bcaaa53a604b3d3f12660e3b7c5cc5
NagendraPatil <nagendrapatil1749@gmail.com>
2024-02-05 23:37:26 +0530

    solved leetcode problem 205 - isomorphic strings

---
+++ Strings/Isomorphic_strings.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function (s, t) {
+    if ([...new Set(s.split(''))].length !== [...new Set(t.split(''))].length) {
+        return false;
+    }
+    let obj1 = {};
+    for (let i = 0; i < s.length; i++) {
+        if (obj1[s[i]] === undefined) {
+            obj1[s[i]] = t[i];
+        } else if (obj1[s[i]] === t[i]) {
+            continue;
+        } else {
+            return false;
+        }
+    }
+    return true;
+};
+
+// Problem 205
