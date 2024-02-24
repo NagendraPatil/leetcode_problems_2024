fceb3c7b7af7c0fc46973151b8b4236d9fa8cf2b
NagendraPatil <nagendrapatil1749@gmail.com>
2024-02-24 22:07:32 +0530

    solved leetcode problem 2023 - number of pair of strings concatination equal to target

---
+++ Arrays/number_of_pair_of_strings_equal_to_target.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} nums
+ * @param {string} target
+ * @return {number}
+ */
+var numOfPairs = function (nums, target) {
+    let count = 0;
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length; j++) {
+            if (i !== j && nums[i] + nums[j] === target) {
+                count++;
+            }
+        }
+    }
+    return count;
+};
+
+//Problem  2023
