adb4a007befc318ee40bb4b07640b8988942a2b5
NagendraPatil <nagendrapatil1749@gmail.com>
2024-03-05 23:56:48 +0530

    solved leetcode problem 1750 - minimum length of string after deleting similar ends

---
+++ Strings/Minimum_length_of_string_after_deletig_similar_ends.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minimumLength = function (s) {
+    s = s.split('');
+    let l = 0; r = s.length - 1;
+    while (l < r) {
+        if (s[l] === s[l + 1] && s[l] === s[r]) {
+            s[l] = '-';
+            l++;
+        }
+        else if (s[r] === s[r - 1] && s[l] === s[r]) {
+            s[r] = '-';
+            r--;
+        }
+        else if (s[l] === s[r]) {
+            s[l] = '-';
+            s[r] = '-';
+            l++;
+            r--;
+        }
+        else {
+            break;
+        }
+    }
+    let count = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] !== '-') {
+            count++;
+        }
+    }
+    return count;
+};
+
+// Problem 1750
